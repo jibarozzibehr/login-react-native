ad6299be89f9d8453feab035c420fe484835a3d6
Juan Ignacio <jibarozzibehr@gmail.com>
2020-04-24 04:08:57 -0300

    TextInput problem partially fixed

--- app/index.js
+++ app/index.js
@@ -129,7 +129,7 @@ class MusicApp extends Component {
 
     render() {
         return (
-            <KeyboardAvoidingView style={{flex:1,backgroundColor:'white',justifyContent: 'flex-end'}} behavior="padding" enabled>
+            <KeyboardAvoidingView style={{flex:1,backgroundColor:'white',justifyContent: 'flex-end'}} behavior={Platform.OS === "ios" ? "padding" : null} enabled>
             
             <View 
                 style={{
